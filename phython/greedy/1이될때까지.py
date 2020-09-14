0f1b3c8baf114655e87a1139ca3fba8933d92b14
swsong89 <ssw890720@gmail.com>
2020-09-15 00:01:30 +0900

    1.그리드 알고리즘 - 파이썬

---
+++ phython/greedy/1이될때까지.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+
+result = 0
+
+while True:
+    if n==1:
+        break
+
+    if n%k == 0:
+        n = n / k
+        result += 1
+    else:
+        n -= 1
+        result += 1
+    print(n)
+
+print(result)
+
