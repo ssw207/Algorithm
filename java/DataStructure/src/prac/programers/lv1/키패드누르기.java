7a0625e10fff087d00787b9c769de0bed558dce4
swsong89 <ssw890720@gmail.com>
2021-12-26 16:07:27 +0900

    1.프로그래머스 1레벨 키패드 문제 리팩토링   - 숫자키 좌표 static으로 변경   - 클릭할 손을 선택하는 메서드 에서 클릭한 손의 좌표를 변경하는 기능 메서드로 분리

--- java/DataStructure/src/prac/programers/lv1/키패드누르기.java
+++ java/DataStructure/src/prac/programers/lv1/키패드누르기.java
@@ -12,19 +12,39 @@ public class 키패드누르기 {
         System.out.println(result);
     }
 
+    //숫자패드 좌표
+    private static int[][] numberPosition = {
+            {1,0}, // 0
+            {0,3}, // 1
+            {1,3}, // 2
+            {2,3}, // 3
+            {0,2}, // 4
+            {1,2}, // 5
+            {2,2}, // 6
+            {0,1}, // 7
+            {1,1}, // 8
+            {2,1} // 9
+    };
+
     public String solution(int[] numbers, String hand) {
         StringBuilder result = new StringBuilder();
         int[] rightHandPosition = {2,0};
         int[] leftHandPosition = {0,0};
 
         for (int number : numbers) {
-            //1)number를 어느 엄지로 누르는지 판단 (입력값 누르는 숫자, 현재위치, 왼손오른손)
+            //1)number를 어느 손으로 누르는지 판단
             String clickHand = getClickHand(number, leftHandPosition, rightHandPosition, hand);
 
-            //2)결과값 StringBuilder에 더하기
-            result.append(clickHand);
+            //2)클릭한 손의 좌표 이동
+            if ("L".equals(clickHand)) {
+                leftHandPosition = numberPosition[number];
+            }  else if ("R".equals(clickHand)) {
+                rightHandPosition = numberPosition[number];
+            }
 
-            System.out.println(String.format("왼:%s, 오:%s, 눌러야할숫자:%s, 사용할손:%s", Arrays.toString(rightHandPosition), Arrays.toString(leftHandPosition), number, clickHand));
+            //3)결과값 StringBuilder에 더하기
+            result.append(clickHand);
+            //System.out.println(String.format("왼:%s, 오:%s, 눌러야할숫자:%s, 사용할손:%s", Arrays.toString(rightHandPosition), Arrays.toString(leftHandPosition), number, clickHand));
         }
 
         //3)결과값리턴
@@ -40,70 +60,34 @@ public class 키패드누르기 {
      * @return
      */
     private String getClickHand(int number, int[] leftHandPosition, int[] rightHandPosition, String hand) {
-        String clickHand = ""; // 클릭한 손
-        int[] targetNumberPosition = getPosition(number); // 입력해야할 숫자 좌표
 
         //왼쪽 키패드
         if (number == 1 || number == 4 || number == 7) {
-            clickHand = "L";
+            return "L";
+        }
 
         //오른쪽 키패드
-        } else  if (number == 3 || number == 6 || number == 9) {
-            clickHand = "R";
+        if (number == 3 || number == 6 || number == 9) {
+            return "R";
+        }
 
         //중앙키패드
-        } else {
-            //현재 손의 위치와 입력한 키패드의 거리
-            int leftHandDistance = getDistance(targetNumberPosition, leftHandPosition);
-            int rightHandDistance = getDistance(targetNumberPosition, rightHandPosition);
+        int[] targetNumberPosition = numberPosition[number]; // 입력해야할 숫자 좌표
 
-            if (leftHandDistance == rightHandDistance) {
-                clickHand = "left".equals(hand) ? "L" : "R";
-            } else if (leftHandDistance < rightHandDistance) {
-                clickHand = "L";
-            } else {
-                clickHand = "R";
-            }
+        //현재 손의 위치와 입력한 키패드의 거리
+        int leftHandDistance = getDistance(targetNumberPosition, leftHandPosition);
+        int rightHandDistance = getDistance(targetNumberPosition, rightHandPosition);
+
+        if (leftHandDistance < rightHandDistance) {
+            return "L";
         }
 
-        //클릭한 손의 좌표 이동
-        if ("L".equals(clickHand)) {
-            leftHandPosition[0] = targetNumberPosition[0];
-            leftHandPosition[1] = targetNumberPosition[1];
-        } else {
-            rightHandPosition[0] = targetNumberPosition[0];
-            rightHandPosition[1] = targetNumberPosition[1];
+        if (leftHandDistance > rightHandDistance) {
+            return "R";
         }
 
-        return clickHand;
-    }
-
-    /**
-     * 입력할 숫자패드의 좌표 조회
-     * @param number
-     * @return
-     */
-    private int[] getPosition(int number) {
-        switch (number) {
-            //왼쪽 키패드
-            case 7 : return new int[] {0,1};
-            case 4 : return new int[] {0,2};
-            case 1 : return new int[] {0,3};
-
-            //중앙 키패드
-            case 0 : return new int[] {1,0};
-            case 8 : return new int[] {1,1};
-            case 5 : return new int[] {1,2};
-            case 2 : return new int[] {1,3};
-            
-            //오른쪽 키패드
-            case 9 : return new int[] {2,1};
-            case 6 : return new int[] {2,2};
-            case 3 : return new int[] {2,3};
-
-            default:
-                throw new IllegalStateException("Unexpected value: " + number);
-        }
+        //같은경우
+        return "left".equals(hand) ? "L" : "R";
     }
 
     /**
