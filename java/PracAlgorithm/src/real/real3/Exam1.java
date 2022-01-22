e18d6846d6a8d7a746c29bbc7cdcdc276cd66192
swsong89 <ssw890720@gmail.com>
2022-01-23 01:58:49 +0900

    1.리팩토링

--- java/PracAlgorithm/src/real/real3/Exam1.java
+++ java/PracAlgorithm/src/real/real3/Exam1.java
@@ -9,15 +9,10 @@ public class Exam1 {
     public static void main(String[] args) {
         Exam1 exam = new Exam1();
         String s = "John Doe, Peter Parker, Mary Jane Watson-Parker, James Doe, John Elvis Doe, Jane Doe, Penny Parker";
-//        String s = "Jane Doe, James Doe, John Doe";
         String rs = exam.solution(s,"Example");
-        System.out.println(rs.equals(s));
 
-        System.out.println(rs);
-        //System.out.println("12345".substring(0,2));
-
-//        "John Doe <jdoe@example.com>, Peter Parker <pparker@example.com>, Mary Jane Watson-Parker <mjwatsonpa@example.com>, James Doe <jdoe2@example.com>, John Elvis Doe <jedoe@example.com>, Jane Doe <jdoe3@example.com>, Penny Parker <pparker2@example.com>"
-//        "John Doe <jdoe@example.com>, Peter Parker <pparker@example.com>, Mary Jane Watson-Parker <mjwatsonpa@example.com>, James Doe <jdoe2@example.com>, John Elvis Doe <jedoe@example.com>, Jane Doe <jdoe2@example.com>, Penny Parker <pparker2@example.com>"
+        String chk = "John Doe <jdoe@example.com>, Peter Parker <pparker@example.com>, Mary Jane Watson-Parker <mjwatsonpa@example.com>, James Doe <jdoe2@example.com>, John Elvis Doe <jedoe@example.com>, Jane Doe <jdoe3@example.com>, Penny Parker <pparker2@example.com>";
+        System.out.println(rs.equals(chk));
     }
 
     /**
@@ -32,39 +27,36 @@ public class Exam1 {
      *  이름 <이메일>,
      */
     public String solution(String names, String compName) {
-        String[] nameArr = names.split(",");
+        StringBuilder result = new StringBuilder(); // 최종결과
+        Map<String, Integer> dupNameChkMap = new HashMap<>(); //중복된 사용자명 수를 체크하기 위한 map
 
-        //중복된 사용자명 수를 체크하기 위한 map
-        Map<String, Integer> dupNameChkMap = new HashMap<>();
-
-        //최종결과
-        StringBuilder result = new StringBuilder();
-
-        for (String name : nameArr) {
-            String mailName = getMailName(name);
-
-            //중복된 이름수
-            Integer cnt = dupNameChkMap.get(mailName);
-
-            if (cnt == null) {
-                dupNameChkMap.put(mailName, 1);
-            } else {
-                dupNameChkMap.put(mailName, (cnt+1));
-                mailName = mailName + (cnt+1);
-            }
+        for (String name : names.split(",")) {
+            //이메일사용자명 구하기
+            String mailName = getDupChekdName(dupNameChkMap, getMailName(name));
 
             // 이메일 만들기
             String email = mailName + "@" + compName.toLowerCase() + ".com";
 
-            System.out.println(result.length());
-            if (result.length() != 0) {
-                result.append(",");
-            }
+            // 맨처음 제외하고 , 붙이기
+            if (result.length() != 0) result.append(",");
 
             // 기존이름 + < 만든이름 + @ + 회사명 + .com)
             result.append(name + " <" + email + ">");
         }
         return result.toString();
+    }
+
+    private String getDupChekdName(Map<String, Integer> dupNameChkMap, String mailName) {
+        //중복된 이름수
+        Integer cnt = dupNameChkMap.get(mailName);
+
+        if (cnt == null) {
+            dupNameChkMap.put(mailName, 1);
+        } else {
+            dupNameChkMap.put(mailName, (cnt+1));
+            mailName = mailName + (cnt+1);
+        }
+        return mailName;
     }
 
     /**
@@ -75,18 +67,10 @@ public class Exam1 {
 
         String[] parts = replacedName.split(" ");
 
-        String first = "";
-        String middle = "";
-        String last = "";
-
-        first = parts[0].substring(0,1);
-
-        if (parts.length == 3) {
-            middle = parts[1].substring(0,1);
-        }
-
-        last = parts[parts.length-1];
-        if (parts[parts.length-1].length() > 8) {
+        String first = parts[0].substring(0,1);
+        String middle = (parts.length == 3) ? parts[1].substring(0,1) : "";
+        String last = parts[parts.length-1];
+        if (last.length() > 8) {
             last = last.substring(0,8);
         }
 
