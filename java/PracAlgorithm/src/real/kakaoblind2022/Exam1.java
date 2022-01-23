d5f400c6b83465ab53972c2b89e7cae54cd8715d
swsong89 <ssw890720@gmail.com>
2022-01-24 00:49:21 +0900

    1.리팩토링

--- java/PracAlgorithm/src/real/kakaoblind2022/Exam1.java
+++ java/PracAlgorithm/src/real/kakaoblind2022/Exam1.java
@@ -12,58 +12,99 @@ public class Exam1 {
         System.out.println(Arrays.toString(rs));
     }
 
+    /**
+     * 15줄 이내 메서드로 분리
+     * 메서드는 단일책임
+     * 1뎁스 이하로
+     */
     public int[] solution(String[] id_list, String[] reports, int k) {//신고자, 신고한사람 신고자, 몆회이상 신고당해야 정지당하는지 판단하는 플래그
-        int[] answer = new int[id_list.length];
-
         //1)각유저가, 누구를 신고했는지 -> 신고처리결과 몇명이 정지됐는지 메일을 보내줘야 하니까요
         //2)각유저가 신고를 몆회당했는지 알아야합니다 .-> 그래 k번이상 신고를 당하면 메일을 보낸것으로 판단하기 때문입니다.
 
-        Map<String, Integer> 동일인중복신고 = new HashMap();
-        Map<String, List<String>> 신고정보 = new HashMap<>(); // 신고당한사람id, {신고자id}
-        Map<String, Integer> 메일발송 = new HashMap<>(); // 신고자id, 메일발송횟수
+        //신고정보 map으로 전환
+        Map<String, List<String>> reportMap = getReportMap(reports);
 
-        for (String report : reports) { // muzi
-            if (동일인중복신고.containsKey(report)) {
+        //신고자 메일발송 정보구하기
+        Map<String, Integer> sendMailMap = getSendMailMap(id_list, k, reportMap);
+
+        //신고자의 메일발송횟수를 저장한 배열 리턴
+        return getSendMailCnt(id_list, sendMailMap);
+    }
+
+    /**
+     * 신고정보를 map으로 반환
+     */
+    private Map<String, List<String>> getReportMap(String[] reports) {
+        Map<String, Integer> dupReportMap = new HashMap();
+        Map<String, List<String>> reportMap = new HashMap<>(); // 신고정보 : key:신고당한사람id, value:{신고자id}
+
+        for (String report : reports) {
+            //존재하면 이미 신고한 정보이므로 스킵
+            if (dupReportMap.containsKey(report)) {
                 continue;
             }
 
-            동일인중복신고.put(report, 0); // muzi frodo
-
             //신고정보 추가
-            String[] rpArr = report.split(" ");
-            String 신고자 = rpArr[0]; // frodo
-            String 신고당한사람 = rpArr[1]; // frodo
-
-            List<String> list = 신고정보.get(신고당한사람);
-            if (list == null) {
-                list = new ArrayList<>();
-            }
-            list.add(신고자);
-            신고정보.put(신고당한사람, list);
+            addReportInfo(reportMap, report);
+            dupReportMap.put(report, 0);
         }
+
+        return reportMap;
+    }
+
+    /**
+     * 신고정보를 map에 추가
+     */
+    private void addReportInfo(Map<String, List<String>> reportMap, String report) {
+        String[] reportInfoArr = report.split(" ");
+        String reporter = reportInfoArr[0]; // frodo
+        String respondent = reportInfoArr[1]; // frodo
+
+        List<String> reporters = reportMap.get(respondent);
+
+        if (reporters == null) reporters = new ArrayList<>(); // 초기화 되지 않은경우 객체 초기화
+
+        reporters.add(reporter);
+        reportMap.put(respondent, reporters);
+    }
+
+    /**
+     * 메일발송정보 map
+     *  @return Map -> key:사용자Id, value:신고메일발송횟수
+     */
+    private Map<String, Integer> getSendMailMap(String[] id_list, int cntMaxReport, Map<String, List<String>> reportMap) {
+        Map<String, Integer> sendMailMap = new HashMap<>(); //신고자id, 메일발송횟수
 
         //신고메일발송
         for (String id : id_list) {
-            List<String> 신고자정보 = 신고정보.get(id);
-            if (신고자정보 == null) continue;
+            List<String> reporters = reportMap.get(id);
+            if (reporters == null) continue;
 
-            //메일발송
-            if (신고자정보.size() >= k) {
+            //신고자수가 cntMaxReport 이상이면 메일발송
+            if (reporters.size() >= cntMaxReport) {
+
                 //신고한사람에게 메일발송함
-                for (String id2 : 신고자정보) {
-                    Integer cnt = 메일발송.get(id2);
-                    메일발송.put(id2, ((cnt == null) ? 0 : cnt) + 1); // 메일발송획수 증가처리
+                for (String reporter : reporters) {
+                    Integer cntSendMail = sendMailMap.get(reporter);
+                    sendMailMap.put(reporter, ((cntSendMail == null) ? 0 : cntSendMail) + 1); // 메일발송횟수 증가처리
                 }
             }
         }
+        return sendMailMap;
+    }
+
+    /**
+     * 사용자별 메일발송 횟수가 담긴 배열 리턴
+     */
+    private int[] getSendMailCnt(String[] id_list, Map<String, Integer> sendMailMap) {
+        int[] sendMailArr = new int[id_list.length];
 
         for (int i = 0; i < id_list.length; i++) {
-            Integer cnt = 메일발송.get(id_list[i]);
-            if (cnt != null) {
-                answer[i] = cnt;
-            }
-        }
+            Integer cnt = sendMailMap.get(id_list[i]);
+            if (cnt == null) continue;
 
-        return answer;
+            sendMailArr[i] = cnt;
+        }
+        return sendMailArr;
     }
 }
