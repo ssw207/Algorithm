4b60bf6c8314c4d4e3587f3d30b2a97d9ac8d6e6
swsong <ssw890720@gmail.com>
2022-08-22 23:48:12 +0900

    프로그래머스 1레벨

---
+++ com/prac/src/programers/lv1/같은숫자는싫어.java
@@ -0,0 +1,34 @@
+package com.prac.src.programers.lv1;
+
+import java.nio.file.StandardWatchEventKinds;
+import java.util.Arrays;
+import java.util.Stack;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12906
+public class 같은숫자는싫어 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {1,1,3,3,0,1,1};
+        int[] result = solution.solution(arr);
+        System.out.println("Arrays.toString(result) = " + Arrays.toString(result)); //1,3,0,1
+    }
+    public static class Solution {
+        public int[] solution(int []arr) {
+            Stack<Integer> stack = new Stack<>();
+            for (int i : arr) {
+                // 마지막 숫자와 다른경우만 넣는다
+                if (stack.empty() || stack.peek() != i) {
+                    stack.add(i);
+                }
+            }
+
+            int[] result = new int[stack.size()];
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                result[size -1 - i] = stack.pop();
+            }
+
+            return result;
+        }
+    }
+}
