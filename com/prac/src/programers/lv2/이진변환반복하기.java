6ccf449fd71f487283ad1bafb4f721cd8d69a7af
swsong <ssw890720@gmail.com>
2022-09-18 23:51:32 +0900

    프로그래머스 2레벨

--- com/prac/src/programers/lv2/이진변환반복하기.java
+++ com/prac/src/programers/lv2/이진변환반복하기.java
@@ -1,8 +1,6 @@
 package com.prac.src.programers.lv2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/70129
 public class 이진변환반복하기 {
@@ -14,33 +12,23 @@ public class 이진변환반복하기 {
 
     static class Solution {
         public int[] solution(String s) {
-            String rs = s;
-            int total = 0;
-            int replace = 0;
+            int[] result = new int[2];
 
-            while (!rs.equals("1")) {
-                //0갯수 확인
-                replace += getOneCount(rs);
-
+            while (!s.equals("1")) {
                 //0을 없앤다
-                int length = rs.replaceAll("0", "").length();
+                String replaced = s.replaceAll("0", "");
+
+                //삭제한 0의 갯수
+                result[1] += s.length() - replaced.length();
 
                 //2진법 변환
-                rs = Integer.toBinaryString(length);
-                total++;
+                s = Integer.toBinaryString(replaced.length());
+                
+                //이진변환 횟수
+                result[0]++;
             }
 
-            return new int[] {total, replace};
-        }
-
-        private int getOneCount(String s) {
-            int cnt = 0;
-            for (char c : s.toCharArray()) {
-                if (c == '0') {
-                    cnt ++;
-                }
-            }
-            return cnt;
+            return result;
         }
     }
 }
