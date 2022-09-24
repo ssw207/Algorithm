b1c56daecf1137791cc2dfcbf36a570fbe8f4a0c
swsong <ssw890720@gmail.com>
2022-09-24 21:06:02 +0900

    프로그래머스 1레벨

---
+++ com/prac/src/programers/lv2/최솟값만들기.java
@@ -0,0 +1,20 @@
+package com.prac.src.programers.lv2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12941
+public class 최솟값만들기 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int solution1 = solution.solution(new int[]{1, 4, 2}, new int[]{5, 4, 4});
+        System.out.println("solution1 = " + solution1);
+    }
+
+    static class Solution {
+        public int solution(int[] A, int[] B) {
+            int answer = 0;
+
+
+            return answer;
+        }
+    }
+
+}
