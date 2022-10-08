4bcb41b11671465ef8d791c100d5b095a094bb63
swsong <ssw890720@gmail.com>
2022-10-08 23:13:24 +0900

    프로그래머스 1레벨

---
+++ com/prac/src/programers/lv1/정수제곱근파별.java
@@ -0,0 +1,21 @@
+package com.prac.src.programers.lv1;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12934
+public class 정수제곱근파별 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        long solution1 = solution.solution(121);
+        System.out.println("solution1 = " + solution1);
+    }
+
+    static class Solution {
+        public long solution(long n) {
+            long pow = (long) (Math.sqrt(n));
+            if ((pow * pow) == n) {
+                return (long) Math.pow(pow + 1, 2);
+            }
+
+            return -1;
+        }
+    }
+}
