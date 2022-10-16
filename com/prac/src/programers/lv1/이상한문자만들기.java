f8693cb953228b04d475d77c8bed5b56c5582e29
swsong <ssw890720@gmail.com>
2022-10-16 16:23:45 +0900

    Changes

--- com/prac/src/programers/lv1/이상한문자만들기.java
+++ com/prac/src/programers/lv1/이상한문자만들기.java
@@ -1,10 +1,5 @@
 package com.prac.src.programers.lv1;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 //https://school.programmers.co.kr/learn/courses/30/lessons/12930
 public class 이상한문자만들기 {
 
@@ -16,19 +11,17 @@ public class 이상한문자만들기 {
 
     static class Solution {
         public String solution(String s) {
-            String words = s.toLowerCase();
-            return Arrays.stream(words.split(" ", -1))
-                    //.peek(System.out::println)
-                    .map(w -> IntStream.range(0, w.length())
-                            .mapToObj(i -> {
-                                if (i % 2 == 0) {
-                                    return (w.charAt(i) + "").toUpperCase();
-                                }
+            String[] split = s.toLowerCase().split("");
 
-                                return w.charAt(i) + "";
-                            })
-                            .collect(Collectors.joining()))
-                    .collect(Collectors.joining(" "));
+            int index = 0;
+            String result = "";
+
+            for (String token : split) {
+                index = (token.equals(" ")) ? 0 : index + 1;
+                result += (index % 2 == 0) ? token : token.toUpperCase();
+            }
+
+            return result;
         }
     }
 }
