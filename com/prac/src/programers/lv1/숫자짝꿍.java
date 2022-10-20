71f136d35a0aaff90c4d814553beb15b70aeaea8
swsong <ssw890720@gmail.com>
2022-10-20 23:47:56 +0900

    Changes

---
+++ com/prac/src/programers/lv1/숫자짝꿍.java
@@ -0,0 +1,16 @@
+package com.prac.src.programers.lv1;
+
+public class 숫자짝꿍 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String solution1 = solution.solution("100",	"2345");
+        System.out.println("solution1 = " + solution1);
+    }
+
+    static class Solution {
+        public String solution(String X, String Y) {
+            String answer = "";
+            return answer;
+        }
+    }
+}
