122e38713d92a2e2ced7b1840b461fb692e3a95b
swsong <ssw890720@gmail.com>
2022-10-23 23:55:21 +0900

    Changes

---
+++ com/prac/src/programers/lv1/시저암호.java
@@ -0,0 +1,22 @@
+package com.prac.src.programers.lv1;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12926
+public class 시저암호 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String solution1 = solution.solution("AB", 1);
+        System.out.println("solution1 = " + solution1);
+    }
+
+    static class Solution {
+        public String solution(String s, int n) {
+            String answer = "";
+
+            String[] arr = {"A","B","C","D","E","F","G","H","I","Z","K","L","M","N","O","P","Q","R","S","T","U","V","W","W","Y","Z"};
+            String[] arr2 = {"a","b","c","d","e","f","g","h","i","z","k","l","m","n","o","p","q","r","s","t","u","v","w","w","y","z"};
+
+            return answer;
+        }
+    }
+}
