b63b51eb8abbfd18652d392f1521cac975ac2dbf
swsong <ssw890720@gmail.com>
2022-10-24 00:30:24 +0900

    Changes

--- com/prac/src/programers/lv1/시저암호.java
+++ com/prac/src/programers/lv1/시저암호.java
@@ -1,22 +1,59 @@
 package com.prac.src.programers.lv1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //https://school.programmers.co.kr/learn/courses/30/lessons/12926
 public class 시저암호 {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String solution1 = solution.solution("AB", 1);
+        String solution1 = solution.solution("AaZz", 25);
         System.out.println("solution1 = " + solution1);
     }
 
     static class Solution {
         public String solution(String s, int n) {
+            Character[] arr = {'A','B','C','D','E','F','G','H','I','Z','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < arr.length; i++) {
+                map.put(arr[i], i);
+            }
+
             String answer = "";
 
-            String[] arr = {"A","B","C","D","E","F","G","H","I","Z","K","L","M","N","O","P","Q","R","S","T","U","V","W","W","Y","Z"};
-            String[] arr2 = {"a","b","c","d","e","f","g","h","i","z","k","l","m","n","o","p","q","r","s","t","u","v","w","w","y","z"};
+            char[] chars = s.toCharArray();
+            for (char c : chars) {
+                if (c == ' ') {
+                    answer += " ";
+                    continue;
+                }
 
+                answer += getNext(c, map, n, arr);
+            }
+            
             return answer;
+        }
+
+        private String getNext(char c, Map<Character, Integer> map, int n, Character[] arr) {
+            boolean isUpperCase = Character.isUpperCase(c);
+
+            if (!isUpperCase) {
+                c = Character.toUpperCase(c);
+            }
+
+            int nextIdx = map.get(c) + n;
+
+            if (nextIdx >= 26) {
+                nextIdx -= 26;
+            }
+
+            if (!isUpperCase) {
+                return (arr[nextIdx] + "").toLowerCase();
+            }
+
+            return arr[nextIdx] + "";
         }
     }
 }
