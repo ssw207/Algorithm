ca22a4c1a837434a3857a144048ddfc83867bbdd
swsong <ssw890720@gmail.com>
2022-10-27 00:08:43 +0900

    Changes

--- com/prac/src/programers/lv2/올바른괄호.java
+++ com/prac/src/programers/lv2/올바른괄호.java
@@ -1,19 +1,20 @@
 package com.prac.src.programers.lv2;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.*;
 
 ///https://school.programmers.co.kr/learn/courses/30/lessons/12909
 public class 올바른괄호 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean solution1 = solution.solution(")()(");
+        boolean solution1 = solution.solution("(())()");
         System.out.println("solution1 = " + solution1);
     }
 
     static class Solution {
         boolean solution(String s) {
+            Map<String, String> map = new HashMap<>();
+            map.put("(",")");
+
             Deque<String> stack = new ArrayDeque<>();
 
             for (String token : s.split("")) {
@@ -22,19 +23,24 @@ public class 올바른괄호 {
                     continue;
                 }
 
+                // 같으면 넣는다.
                 String peek = stack.peek();
-                
-                // 같으면 넣는다
                 if (peek.equals(token)) {
-                    stack.add(peek);
+                    stack.add(token);
                     continue;
                 }
 
-                // 다르면 뺀다
-                stack.pop();
+                // 쌍이면 뺀다
+                getReverse(map, peek)
+                        .filter(pair -> pair.equals(token))
+                        .ifPresent(pair -> stack.pop());
             }
 
             return stack.isEmpty();
+        }
+
+        private Optional<String> getReverse(Map<String, String> map, String token) {
+            return Optional.ofNullable(map.get(token));
         }
     }
 }
