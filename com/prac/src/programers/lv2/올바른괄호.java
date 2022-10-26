4c8339117678d509c2cc717c7ac78194f5b6160e
swsong <ssw890720@gmail.com>
2022-10-27 00:23:22 +0900

    Changes

--- com/prac/src/programers/lv2/올바른괄호.java
+++ com/prac/src/programers/lv2/올바른괄호.java
@@ -2,45 +2,46 @@ package com.prac.src.programers.lv2;
 
 import java.util.*;
 
-///https://school.programmers.co.kr/learn/courses/30/lessons/12909
 public class 올바른괄호 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean solution1 = solution.solution("(())()");
+        boolean solution1 = solution.solution("()()");
         System.out.println("solution1 = " + solution1);
     }
 
     static class Solution {
         boolean solution(String s) {
-            Map<String, String> map = new HashMap<>();
-            map.put("(",")");
+            Deque<Character> stack = new ArrayDeque<>();
 
-            Deque<String> stack = new ArrayDeque<>();
-
-            for (String token : s.split("")) {
+            for (char token : s.toCharArray()) {
+                // 초기값이면 넣는다
                 if (stack.isEmpty()) {
                     stack.add(token);
                     continue;
                 }
 
+                char peek = stack.peek();
+                if (')' == peek && '(' == token) {
+                    return false;
+                }
+
                 // 같으면 넣는다.
-                String peek = stack.peek();
-                if (peek.equals(token)) {
+                if (peek==(token)) {
                     stack.add(token);
                     continue;
                 }
 
                 // 쌍이면 뺀다
-                getReverse(map, peek)
-                        .filter(pair -> pair.equals(token))
-                        .ifPresent(pair -> stack.pop());
+                if ('(' == stack.peek() && ')'== token) {
+                    stack.pop();
+                    continue;
+                }
+
+                stack.add(token);
             }
 
             return stack.isEmpty();
         }
-
-        private Optional<String> getReverse(Map<String, String> map, String token) {
-            return Optional.ofNullable(map.get(token));
-        }
     }
 }
+///https://school.programmers.co.kr/learn/courses/30/lessons/12909
