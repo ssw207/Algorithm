6da5e732024a7596abbdda0df1951dc1822a89f5
swsong <ssw890720@gmail.com>
2022-11-06 23:55:23 +0900

    Changes

---
+++ com/prac/src/programers/lv1/삼총사.java
@@ -0,0 +1,18 @@
+package com.prac.src.programers.lv1;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/131705
+public class 삼총사 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int solution1 = solution.solution(new int[] {-2, 3, 0, 2, -5});
+        System.out.println("solution1 = " + solution1);
+    }
+
+    static class Solution {
+        public int solution(int[] number) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
