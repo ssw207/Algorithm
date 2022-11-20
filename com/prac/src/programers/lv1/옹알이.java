226f02f6ad27c964ad304c093b85292bbd0af8dd
swsong <ssw890720@gmail.com>
2022-11-20 19:59:17 +0900

    Changes

--- com/prac/src/programers/lv1/옹알이.java
+++ com/prac/src/programers/lv1/옹알이.java
@@ -1,20 +1,18 @@
 package com.prac.src.programers.lv1;
 
-import javax.management.BadAttributeValueExpException;
-import java.util.Set;
-
 //https://school.programmers.co.kr/learn/courses/30/lessons/133499
 public class 옹알이 {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         //int solution1 = solution.solution(new String[] {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"});
-        int solution1 = solution.solution(new String[] {"ayaye"});
+        int solution1 = solution.solution(new String[]{"wooyemawooye"});
         System.out.println("solution1 = " + solution1);
     }
 
     static class Solution {
         private String[] strings = {"aya", "ye", "woo", "ma"};
+        private String[] notValidStrs = {"ayaaya", "yeye", "woowoo", "mama"};
 
         public int solution(String[] babbling) {
             int answer = 0;
@@ -28,9 +26,21 @@ public class 옹알이 {
         }
 
         private boolean isValidStr(String str) {
-            for (String string : strings) {
-                str = str.replaceFirst(string, "");
-                //System.out.println("str = " + str);
+            for (String notValidStr : notValidStrs) {
+                if (str.contains(notValidStr)) {
+                    return false;
+                }
+            }
+
+            int before = str.length();
+            int after = 0;
+            while (before != after) {
+                for (String string : strings) {
+                    before = str.length();
+                    str = str.replaceFirst(string, "");
+                    after = str.length();
+                    //System.out.println("str = " + str);
+                }
             }
 
             return str.equals("");
